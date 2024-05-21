711b9eebd78ec95a681b5bee058df0c743e1d425
Fredrik Nordström <livog@live.com>
2024-05-21 22:37:27 +0200

    add sign up button to header

--- src/components/Header/SignInOrProfile.tsx
+++ src/components/Header/SignInOrProfile.tsx
@@ -2,20 +2,29 @@ import { hasAuthCookie } from '@/lib/auth/edge'
 import { headers } from 'next/headers'
 import Link from 'next/link'
 import { Suspense } from 'react'
-import { Skeleton } from '../ui/Skeleton'
+import { Skeleton } from '@/components/ui/Skeleton'
 import ProfileMenu from './ProfileMenu'
+import authConfig from '@/lib/auth/config'
 
 const SignInOrProfile = () => {
   const isSignedIn = hasAuthCookie(headers())
   return (
-    <div className="relative">
+    <div className="relative flex gap-x-2">
       {!isSignedIn && (
-        <Link
-          className="rounded-full bg-zinc-200 px-5 py-3 font-medium text-black transition-all duration-300 hover:bg-zinc-300 dark:bg-zinc-800 dark:text-white
-            dark:hover:bg-zinc-700"
-          href="/sign-in">
-          Sign in
-        </Link>
+        <>
+          <Link
+            className="rounded-full bg-zinc-800 px-5 py-2.5 font-medium text-white transition-all duration-300 hover:bg-zinc-300 dark:bg-white dark:text-black
+              dark:hover:bg-zinc-200"
+            href={authConfig?.pages?.newUser || '/sign-in'}>
+            Try for free
+          </Link>
+          <Link
+            className="rounded-full bg-zinc-200 px-5 py-2.5 font-medium text-black transition-all duration-300 hover:bg-zinc-300 dark:bg-zinc-800
+              dark:text-white dark:hover:bg-zinc-700"
+            href={authConfig?.pages?.signIn || '/sign-up'}>
+            Sign in
+          </Link>
+        </>
       )}
       {isSignedIn ? (
         <Suspense
